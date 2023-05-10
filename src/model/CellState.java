936c7fdacca45a3447719409e01d098b723107d1
Simone <74970157+Robotkiller001@users.noreply.github.com>
2023-05-10 20:28:21 +0200

    Possibile idea ma non sono sicuro

--- src/model/CellState.java
+++ src/model/CellState.java
@@ -12,5 +12,6 @@ public abstract class CellState extends Cell implements Interactable
         this.state = state;
     }
 
-    abstract public Cell Event(Map mappa) throws CantGenerateStateEventException;
+    abstract public void Event(Map mappa) throws CantGenerateStateEventException;
+    //abstract public Cell Event(Map mappa) throws CantGenerateStateEventException;
 }
--- src/model/element/Sink.java
+++ src/model/element/Sink.java
@@ -51,10 +51,8 @@ public class Sink extends CellState
     }
 
     @Override
-    public Cell Event(Map mappa) throws CantGenerateStateEventException
+    public void Event(Map mappa) throws CantGenerateStateEventException 
     {
-        Cell c = null;
-
         if (this.state == "base") {
             Timer = new Timer();
             Timer.schedule(new TimerTask() {
@@ -67,26 +65,46 @@ public class Sink extends CellState
         }
         else
         {
-            if(i < mappa.getISize() - 1 && mappa.getCasella(this.i + 1, this.j) == null)
-            {
-                c = new Water(this.i + 1, this.j);
+            if (this.state == "broken") {
+                Timer = new Timer();
+                Timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        try {
+                            createWater(mappa);
+                        } catch (CantGenerateStateEventException e) {
+                            e.printStackTrace();
+                        }
+                        Timer.cancel();
+                    }
+                }, 0, TIME);
             }
-            else if(j < mappa.getJSize() -1 && mappa.getCasella(this.i, this.j + 1) == null)
-            {
-                c = new Water(this.i, this.j + 1);
-            }
-            else if(i > 1 && mappa.getCasella(this.i - 1, this.j) == null)
-            {
-                c = new Water(this.i - 1, this.j);
-            }
-            else if(j > 1 && mappa.getCasella(this.i + 1, this.j - 1) == null)
-            {
-                c = new Water(this.i, this.j - 1);
-            }
-                else
-            {
-                throw new CantGenerateStateEventException("All surrounding cell are full");
-            }
+        }
+        
+    }
+
+    public Cell createWater(Map mappa) throws CantGenerateStateEventException{
+        Cell c = null;
+
+        if(i < mappa.getISize() - 1 && mappa.getCasella(this.i + 1, this.j) == null)
+        {
+            c = new Water(this.i + 1, this.j);
+        }
+        else if(j < mappa.getJSize() -1 && mappa.getCasella(this.i, this.j + 1) == null)
+        {
+            c = new Water(this.i, this.j + 1);
+        }
+        else if(i > 1 && mappa.getCasella(this.i - 1, this.j) == null)
+        {
+            c = new Water(this.i - 1, this.j);
+        }
+        else if(j > 1 && mappa.getCasella(this.i + 1, this.j - 1) == null)
+        {
+            c = new Water(this.i, this.j - 1);
+        }
+        else
+        {
+            throw new CantGenerateStateEventException("All surrounding cell are full");
         }
         return c;
     }

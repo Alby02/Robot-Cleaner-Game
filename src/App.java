69314a977a8e873a9ccba737a6ac5f47df9319bf
Simone <74970157+Robotkiller001@users.noreply.github.com>
2023-05-17 19:35:17 +0200

    boh

--- src/App.java
+++ src/App.java
@@ -2,6 +2,9 @@
 import controller.WhereIAmController;
 import model.Map;
 import model.MapBuilder;
+import model.element.Sink;
+import model.timer.EventTimer;
+import model.timer.SinkTimer;
 import view.GUIPartialView;
 import view.GUIView;
 import view.TextualView;
@@ -20,9 +23,13 @@ public class App {
             TextualView view2 = new TextualView();
             GUIPartialView view3 = new GUIPartialView(map, view);
             WhereIAmController contro = new WhereIAmController(map, view, view3, view2);
+            Sink sink = new Sink (0, 0);
+            SinkTimer sinkTimer = new SinkTimer(0, 10000);
+            sinkTimer.startTimer();
             //TODO add thred for random event (passare mappa)
         } catch (Exception e) {
             System.err.println(e.getMessage());
-        }    
+        }
+        sinkTimer.stopTimer();
     }
 }
